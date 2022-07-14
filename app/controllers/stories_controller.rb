2c7f82d99f2f95a308afaff4b7e6e6c6e53bb5eb
marko-kolosek1 <markok@kolosek.com>
2022-07-14 15:24:18 +0200

    [#182140094]added Story model with attributes, crud actions, created scope for access by users

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,60 @@
+class StoriesController < ApplicationController
+  before_action :set_story, except: [:index, :new, :create]
+  before_action :set_last_edited, only: [:update], if: :user_signed_in?
+  
+  def index
+    if current_user.role == "admin" || current_user.role == "editor"
+      @stories = Story.all
+    elsif current_user.role == "author"
+      @stories = Story.author_stories(current_user)      
+    end
+  end
+
+  def show
+    @story
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.user_id = current_user.id
+    @story.save
+
+    respond_to do |format|
+      format.html { redirect_to user_stories_path, notice: "Story was successfully destroyed." }
+    end   
+   end
+
+  def update
+    @story.update(story_params)
+
+    respond_to do |format|
+      format.html { redirect_to user_stories_path, notice: "Story was successfully destroyed." }
+    end
+  end
+
+  def destroy
+    @story.destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_stories_path, notice: "Story was successfully destroyed." }
+    end
+  end
+
+  private
+
+  def set_story
+    @story = Story.find(params[:id])
+  end
+
+  def set_last_edited
+    @story.update_attribute(:last_edited, Time.current)
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_112749) do
+ActiveRecord::Schema.define(version: 2022_07_13_132234) do
+
+  create_table "stories", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "last_edited"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stories_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,4 +52,5 @@ ActiveRecord::Schema.define(version: 2022_07_13_112749) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "stories", "users"
 end
