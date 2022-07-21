db0d82f2aeee67006f0acd77409644bb9756f769
marko-kolosek1 <markok@kolosek.com>
2022-07-21 09:01:07 +0200

    [#182140087]fix tests for get show stories

--- spec/api/users_controller_api_spec.rb
+++ spec/api/users_controller_api_spec.rb
@@ -25,13 +25,13 @@ RSpec.describe Api::V1::UsersController do
       get "/api/v1/users/#{@user.id}"
       expect(response).to have_http_status(:unauthorized)      
     end
-    it "see user stories without access token" do
-      get "/api/v1/users/#{@user.id}", headers: {Authorization: "Token token=#{@api_key.access_token}"}
-      expect(response).to have_http_status(:unauthorized)
-    end
     it "see user stories with wrong access token" do
       get "/api/v1/users/#{@user.id}", headers: {Authorization: "Token token=wrongtoken1234567"}
       expect(response).to have_http_status(:unauthorized)
+    end
+    it "see user stories with correct access token" do
+      get "/api/v1/users/#{@user.id}", headers: {Authorization: "Token token=#{@api_key.access_token}"}
+      expect(response).to have_http_status(:success)
     end    
   end
 end
