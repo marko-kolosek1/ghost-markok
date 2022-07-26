e834830eec0ccebd2cbd030584dd6e767b094df8
marko-kolosek1 <markok@kolosek.com>
2022-07-26 12:48:11 +0200

    fixed policy class name

--- app/policies/setting_policy.rb
+++ app/policies/setting_policy.rb
@@ -1,4 +1,4 @@
-class UserPolicy < ApplicationPolicy
+class SettingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
